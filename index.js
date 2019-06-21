5c24bc2b8ee560b56cfd3183461b7d49d25c2233
Nikole Hache <nh131215@gmail.com>
2019-06-21 17:38:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function add2(n) {
   return n + two
 }
 
-function funkyFunction() {
+var funkyFunction = function() {
   return function() {
     return "FUNKY!"
   }
@@ -25,8 +25,6 @@ function funkyFunction() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = function() {
-  return function() {
+function theFunk() {
     return "FUNKY!" 
-  }
 }
